10e9bbd96f05b659d8b05e47b21d7c6205c443fb
vinicius <noefvini@gmail.com>
2026-01-10 20:53:57 +0000

    add transaction repository

---
+++ payment-system-api/src/main/java/com/smartpaymentsystem/repository/TransactionRepository.java
@@ -0,0 +1,29 @@
+package com.smartpaymentsystem.repository;
+
+import com.smartpaymentsystem.domain.Transaction;
+import com.smartpaymentsystem.domain.TransactionType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.Instant;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Page<Transaction> findByBusinessId(Long businessId, Pageable pageable);
+
+    Page<Transaction> findByBusinessIdAndType(Long businessId, TransactionType type, Pageable pageable);
+
+    Page<Transaction> findByBusinessIdAndOccurredAtBetween(
+            Long businessId,
+            Instant start,
+            Instant end,
+            Pageable pageable
+    );
+
+    Page<Transaction> findByBusinessIdAndTypeAndOccurredAtBetween(
+            Long businessId,
+            TransactionType type,
+            Instant start,
+            Instant end,
+            Pageable pageable
+    );
+}
