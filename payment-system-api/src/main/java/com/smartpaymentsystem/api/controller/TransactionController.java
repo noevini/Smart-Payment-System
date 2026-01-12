cd25ae836ceb50b13675aceb75f8bf5c8ce567c1
vinicius <noefvini@gmail.com>
2026-01-12 17:51:11 +0000

    add transaction controller

---
+++ payment-system-api/src/main/java/com/smartpaymentsystem/api/controller/TransactionController.java
@@ -0,0 +1,49 @@
+package com.smartpaymentsystem.api.controller;
+
+import com.smartpaymentsystem.api.dto.TransactionRequestDTO;
+import com.smartpaymentsystem.api.dto.TransactionResponseDTO;
+import com.smartpaymentsystem.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/businesses/{businessId}/transactions")
+@AllArgsConstructor
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    @GetMapping
+    public Page<TransactionResponseDTO> getTransactions(@PathVariable Long businessId, Pageable pageable) {
+        return transactionService.getByBusiness(businessId, pageable);
+    }
+
+    @GetMapping("{transactionId}")
+    public TransactionResponseDTO getTransactionById(@PathVariable Long transactionId, @PathVariable Long businessId ) {
+        return transactionService.getById(transactionId, businessId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransactionResponseDTO createTransaction(@PathVariable Long businessId, @Valid @RequestBody TransactionRequestDTO requestDTO) {
+        requestDTO.setBusinessId(businessId);
+
+        return transactionService.createTransaction(requestDTO);
+    }
+
+    @PutMapping("/{transactionId}")
+    public TransactionResponseDTO updateTransaction(@PathVariable Long transactionId, @PathVariable Long businessId, @Valid @RequestBody TransactionRequestDTO requestDTO) {
+        requestDTO.setBusinessId(businessId);
+
+        return transactionService.updateTransaction(transactionId, businessId, requestDTO);
+    }
+
+    @DeleteMapping("/{transactionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTransaction(@PathVariable Long transactionId, @PathVariable Long businessId) {
+        transactionService.deleteTransaction(transactionId, businessId);
+    }
+}
