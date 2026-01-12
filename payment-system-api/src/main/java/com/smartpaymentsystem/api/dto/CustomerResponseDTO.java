53f15e0adb96c4c18f84abd0be5bce7d16c2fba7
vinicius <noefvini@gmail.com>
2026-01-12 21:14:21 +0000

    add customer response dto

---
+++ payment-system-api/src/main/java/com/smartpaymentsystem/api/dto/CustomerResponseDTO.java
@@ -0,0 +1,19 @@
+package com.smartpaymentsystem.api.dto;
+
+import com.smartpaymentsystem.domain.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+public class CustomerResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String notes;
+    private Instant createdAt;
+    private Instant updatedAt;
+}
