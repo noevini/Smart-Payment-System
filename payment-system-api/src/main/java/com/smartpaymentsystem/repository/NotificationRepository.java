5666e2472fbd83b0c6864faaf2661ec8315b8bfa
vinicius <noefvini@gmail.com>
2026-02-19 22:41:44 +0000

    add exists method to prevent duplicate overdue notifications

--- payment-system-api/src/main/java/com/smartpaymentsystem/repository/NotificationRepository.java
+++ payment-system-api/src/main/java/com/smartpaymentsystem/repository/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.smartpaymentsystem.repository;
 
 import com.smartpaymentsystem.domain.Notification;
+import com.smartpaymentsystem.domain.NotificationType;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     Page<Notification> findByBusiness_id(Long businessId, Pageable pageable);
 
     Optional<Notification> findByIdAndBusiness_Id(Long id, Long businessId);
+
+    boolean existsByBusiness_IdAndTypeAndRelatedEntityTypeAndRelatedEntityId(
+            Long business_id, @NotNull NotificationType type, String relatedEntityType, Long relatedEntityId
+    );
 }
