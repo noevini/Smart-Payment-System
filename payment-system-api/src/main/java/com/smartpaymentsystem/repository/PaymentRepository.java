8bd10c1981d21fce5937b1fb03ec611e91d9d81c
vinicius <noefvini@gmail.com>
2026-02-19 22:42:57 +0000

    add method to find overdue payments

--- payment-system-api/src/main/java/com/smartpaymentsystem/repository/PaymentRepository.java
+++ payment-system-api/src/main/java/com/smartpaymentsystem/repository/PaymentRepository.java
@@ -1,12 +1,15 @@
 package com.smartpaymentsystem.repository;
 
 import com.smartpaymentsystem.domain.Payment;
+import com.smartpaymentsystem.domain.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByBusiness_Id(Long businessId);
     Optional<Payment> findByIdAndBusiness_Id(Long paymentId, Long businessId);
+    List<Payment> findByStatusNotAndDueDateBefore(PaymentStatus status, Instant now);
 }
