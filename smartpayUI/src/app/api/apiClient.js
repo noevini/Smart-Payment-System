53dd033e5186093e519970efd8623e0ab88cc588
vinicius <noefvini@gmail.com>
2026-02-23 17:48:28 +0000

    add auth header interceptor to api client

--- smartpayUI/src/app/api/apiClient.js
+++ smartpayUI/src/app/api/apiClient.js
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { getToken } from "../auth/tokenStorage";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+});
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
