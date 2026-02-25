a15d1316617234d810baef90d2be35668fdac5a9
vinicius <noefvini@gmail.com>
2026-02-25 21:21:51 +0000

    protect routes using jwt token

--- smartpayUI/src/app/RequireAuth.jsx
+++ smartpayUI/src/app/RequireAuth.jsx
@@ -1,6 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 export default function RequireAuth({ children }) {
-  const ok = localStorage.getItem("demo_auth") === "true";
-  return ok ? children : <Navigate to="/login" replace />;
+  const token = localStorage.getItem("smartpay_token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 }
