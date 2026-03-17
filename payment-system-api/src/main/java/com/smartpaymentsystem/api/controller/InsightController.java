e3b70588ecccb0a232db23e3c091047f5ee6a01c
vinicius <noefvini@gmail.com>
2026-03-17 20:55:08 +0000

    add InsightController summary endpoint

---
+++ payment-system-api/src/main/java/com/smartpaymentsystem/api/controller/InsightController.java
@@ -0,0 +1,18 @@
+package com.smartpaymentsystem.api.controller;
+
+import com.smartpaymentsystem.api.dto.InsightResponseDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/businesses/{businessId}/insights")
+@AllArgsConstructor
+public class InsightController {
+    private final InsightService insightService;
+
+    public InsightResponseDTO getSummary(@PathVariable Long businessId) {
+        return insightService.generateSummary(businessId);
+    }
+}
