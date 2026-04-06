11d9c9d537db6966ad5c0f2b4380aed4c2452668
vinicius <noefvini@gmail.com>
2026-04-06 20:58:33 +0100

    add missing @GetMapping to InsightController

--- payment-system-api/src/main/java/com/smartpaymentsystem/api/controller/InsightController.java
+++ payment-system-api/src/main/java/com/smartpaymentsystem/api/controller/InsightController.java
@@ -3,6 +3,7 @@ package com.smartpaymentsystem.api.controller;
 import com.smartpaymentsystem.api.dto.InsightResponseDTO;
 import com.smartpaymentsystem.service.InsightService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class InsightController {
     private final InsightService insightService;
 
+    @GetMapping("/summary")
     public InsightResponseDTO getSummary(@PathVariable Long businessId) {
         return insightService.generateSummary(businessId);
     }
