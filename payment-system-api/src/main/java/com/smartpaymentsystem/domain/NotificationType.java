55780e78506b81b8cc9319c1b01d1f4fcc4f983b
vinicius <noefvini@gmail.com>
2026-04-08 22:53:15 +0100

    expand NotificationType with PAYMENT_PAID and PAYMENT_DUE_SOON

--- payment-system-api/src/main/java/com/smartpaymentsystem/domain/NotificationType.java
+++ payment-system-api/src/main/java/com/smartpaymentsystem/domain/NotificationType.java
@@ -1,5 +1,7 @@
 package com.smartpaymentsystem.domain;
 
 public enum NotificationType {
-    PAYMENT_OVERDUE
+    PAYMENT_OVERDUE,
+    PAYMENT_PAID,
+    PAYMENT_DUE_SOON  // For future scheduler: warn X days before due date
 }
